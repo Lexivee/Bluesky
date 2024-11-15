3d4a22b3a30f53a8798ccc05059a8fbda49d312a
khuddite <biliie811028@hotmail.com>
2024-11-15 13:12:46 -0500

    Implement manual truncation logic for number formatting on mobile

--- src/view/com/util/numeric/format.ts
+++ src/view/com/util/numeric/format.ts
@@ -1,12 +1,24 @@
 import type {I18n} from '@lingui/core'
 
 export const formatCount = (i18n: I18n, num: number) => {
-  return i18n.number(num, {
+  const options: Intl.NumberFormatOptions = {
     notation: 'compact',
     maximumFractionDigits: 1,
-    // `1,953` shouldn't be rounded up to 2k, it should be truncated.
-    // @ts-expect-error: `roundingMode` doesn't seem to be in the typings yet
-    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/NumberFormat/NumberFormat#roundingmode
-    roundingMode: 'trunc',
-  })
+  }
+
+  // `1,953` shouldn't be rounded up to 2k, it should be truncated.
+  if ('roundingMode' in Intl.NumberFormat.prototype) {
+    options.roundingMode = 'trunc'
+  } else {
+    // if 'roundingMode' is not supported in the current runtime, fall back to manual truncation
+    if (num >= 1e9) {
+      num = Math.floor(num / 1e8) * 1e8
+    } else if (num >= 1e6) {
+      num = Math.floor(num / 1e5) * 1e5
+    } else if (num >= 1000) {
+      num = Math.floor(num / 1e2) * 1e2
+    }
+  }
+
+  return i18n.number(num, options)
 }
