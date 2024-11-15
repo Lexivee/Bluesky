8e3784bba5f742f44736f6811868fb86335cd7b9
khuddite <biliie811028@hotmail.com>
2024-11-15 13:44:19 -0500

    Simplify truncation logic

--- src/view/com/util/numeric/format.ts
+++ src/view/com/util/numeric/format.ts
@@ -1,24 +1,24 @@
 import type {I18n} from '@lingui/core'
 
 export const formatCount = (i18n: I18n, num: number) => {
+  let truncatedNum
   const options: Intl.NumberFormatOptions = {
     notation: 'compact',
     maximumFractionDigits: 1,
   }
 
   // `1,953` shouldn't be rounded up to 2k, it should be truncated.
-  if ('roundingMode' in Intl.NumberFormat.prototype) {
-    options.roundingMode = 'trunc'
+  if (num >= 1e12) {
+    truncatedNum = Math.floor(num / 1e11) * 1e11
+  } else if (num >= 1e9) {
+    truncatedNum = Math.floor(num / 1e8) * 1e8
+  } else if (num >= 1e6) {
+    truncatedNum = Math.floor(num / 1e5) * 1e5
+  } else if (num >= 1e3) {
+    truncatedNum = Math.floor(num / 1e2) * 1e2
   } else {
-    // if 'roundingMode' is not supported in the current runtime, fall back to manual truncation
-    if (num >= 1e9) {
-      num = Math.floor(num / 1e8) * 1e8
-    } else if (num >= 1e6) {
-      num = Math.floor(num / 1e5) * 1e5
-    } else if (num >= 1000) {
-      num = Math.floor(num / 1e2) * 1e2
-    }
+    truncatedNum = num
   }
 
-  return i18n.number(num, options)
+  return i18n.number(truncatedNum, options)
 }
